12c0e2ef6097b3a7ba9a63b684a7d08d57f02254
Ilan Ganor <deadpoolcode1@gmail.com>
2023-06-30 18:09:12 +0700

    Update commands to test based on requirement

--- src/main.c
+++ src/main.c
@@ -12,10 +12,42 @@ TIM_HandleTypeDef htim3;
 
 void Error_Handler(void)
 {
+  printk("Go there\r\n");
   __disable_irq();
   while (1)
   {
   }
+}
+
+void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* htim_pwm)
+{
+  if(htim_pwm->Instance==TIM1)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM1_CLK_DISABLE();
+  }
+
+}
+
+/**
+* @brief TIM_Base MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param htim_base: TIM_Base handle pointer
+* @retval None
+*/
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
+{
+  if(htim_base->Instance==TIM2)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM2_CLK_DISABLE();
+  }
+  else if(htim_base->Instance==TIM3)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM3_CLK_DISABLE();
+  }
+
 }
 
 void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
@@ -23,15 +55,6 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
   if(htim_pwm->Instance==TIM1)
   {
-  /** Initializes the peripherals clocks
-  */
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_TIM1;
-    PeriphClkInit.Tim1ClockSelection = RCC_TIM1CLKSOURCE_PCLK1;
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-    {
-      Error_Handler();
-    }
-
     /* Peripheral clock enable */
     __HAL_RCC_TIM1_CLK_ENABLE();
   }
@@ -98,16 +121,24 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
 
 }
 
-static void MX_TIM1_Init(void)
+static void MX_TIM1_Init(int prescaler, int period, int pwm_pulse, int oc)
 {
+  static bool is_config = false;
+  if (is_config) {
+    /* Deinit the PWM */
+    printk("Deinit for TIM1\r\n");
+    HAL_TIM_PWM_DeInit(&htim1);
+    HAL_TIM_OC_DeInit(&htim1);
+  }
+  is_config = true;
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 19;
+  htim1.Init.Prescaler = prescaler;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 9;
+  htim1.Init.Period = period;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -127,7 +158,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 5;
+  sConfigOC.Pulse = pwm_pulse;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
@@ -138,7 +169,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_ACTIVE;
-  sConfigOC.Pulse = 3;
+  sConfigOC.Pulse = oc;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
@@ -165,16 +196,24 @@ static void MX_TIM1_Init(void)
 
 }
 
-static void MX_TIM2_Init(void)
+static void MX_TIM2_Init(int prescaler, int period, int pwm_pulse, int oc)
 {
   TIM_SlaveConfigTypeDef sSlaveConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
-
+  static bool is_config = false;
+  if (is_config) {
+    /* Deinit the PWM */
+    printk("Deinit for TIM2\r\n");
+    HAL_TIM_Base_DeInit(&htim2);
+    HAL_TIM_PWM_DeInit(&htim2);
+    HAL_TIM_OC_DeInit(&htim2);
+  }
+  is_config = true;
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 19;
+  htim2.Init.Prescaler = prescaler;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 9;
+  htim2.Init.Period = period;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -202,7 +241,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 5;
+  sConfigOC.Pulse = pwm_pulse;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
@@ -210,7 +249,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_ACTIVE;
-  sConfigOC.Pulse = 3;
+  sConfigOC.Pulse = oc;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_OC_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
@@ -222,16 +261,23 @@ static void MX_TIM2_Init(void)
 
 }
 
-static void MX_TIM3_Init(void)
+static void MX_TIM3_Init(int prescaler, int period, int pwm_pulse, int oc)
 {
   TIM_SlaveConfigTypeDef sSlaveConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
-
+  static bool is_config = false;
+  if (is_config) {
+    /* Deinit the PWM */
+    printk("Deinit for TIM3\r\n");
+    HAL_TIM_PWM_DeInit(&htim3);
+    HAL_TIM_Base_DeInit(&htim3);
+  }
+  is_config = true;
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 19;
+  htim3.Init.Prescaler = prescaler;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 9;
+  htim3.Init.Period = period;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -255,7 +301,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 5;
+  sConfigOC.Pulse = pwm_pulse;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
@@ -269,12 +315,109 @@ static void MX_TIM3_Init(void)
 
 void main(void)
 {
-	MX_TIM1_Init();
-	MX_TIM2_Init();
-	MX_TIM3_Init();
+
+}
+
+#include <zephyr/shell/shell.h>
+int cpu_hz = 50E6;
+int max_shift = 360;
+int current_period = 10;
+int current_freq = 250000;
+int current_pulse = 50;
+int current_shift = 120;
+
+static int cmd_set_freq(const struct shell *shell, size_t argc, char **argv)
+{
+  int freq = atoi(argv[1]);
+  int prescaler = cpu_hz / (current_period * freq)  - 1;
+  int pulse = 50 * current_period / 100;
+  int shift = (current_period - 1) * current_shift / max_shift;
+  shell_print(shell, "PWM Parameter %d %d %d", prescaler, pulse, shift);
+	MX_TIM1_Init(prescaler, current_period - 1, pulse, shift);
+	MX_TIM2_Init(prescaler, current_period - 1, pulse, shift);
+	MX_TIM3_Init(prescaler, current_period - 1, pulse, shift);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+
+  current_freq = freq;
+	return 0;
 }
+
+static int cmd_get_shift(const struct shell *shell, size_t argc, char **argv)
+{
+  int shift = atoi(argv[1]);
+  int prescaler = cpu_hz / (current_period * current_freq)  - 1;
+  int pulse = 50 * current_period / 100;
+  int oc = (current_period - 1) * shift / max_shift;
+  shell_print(shell, "PWM Parameter %d %d %d", prescaler, pulse, oc);
+  k_sleep(K_SECONDS(1));
+	MX_TIM1_Init(prescaler, current_period - 1, pulse, oc);
+	MX_TIM2_Init(prescaler, current_period - 1, pulse, oc);
+	MX_TIM3_Init(prescaler, current_period - 1, pulse, oc);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+
+  current_shift = shift;
+	return 0;
+}
+
+static int cmd_set_start(const struct shell *shell, size_t argc, char **argv)
+{
+  int prescaler = cpu_hz / (current_period * current_freq)  - 1;
+  int pulse = 50 * current_period / 100;
+  int shift = (current_period - 1) * current_shift / max_shift;
+  shell_print(shell, "PWM Parameter %d %d %d", prescaler, pulse, shift);
+	MX_TIM1_Init(prescaler, current_period - 1, pulse, shift);
+	MX_TIM2_Init(prescaler, current_period - 1, pulse, shift);
+	MX_TIM3_Init(prescaler, current_period - 1, pulse, shift);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+	return 0;
+}
+
+static int cmd_get_stop(const struct shell *shell, size_t argc, char **argv)
+{
+  HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_OC_Stop(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_OC_Stop(&htim2, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
+	return 0;
+}
+
+static int cmd_get_config(const struct shell *shell, size_t argc, char **argv)
+{
+  shell_print(shell, "Frequency %d - Pulse %d - Shift %d", current_freq, 50, current_shift);
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_pwm,
+	SHELL_CMD(config, NULL, "Get current configuration", cmd_get_config),
+	SHELL_CMD(freq, NULL, "Set the frequency", cmd_set_freq),
+	SHELL_CMD(shift, NULL, "Set the shift degree", cmd_get_shift),
+	SHELL_SUBCMD_SET_END);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_test,
+	SHELL_CMD(start, NULL, "Start generating the PWM pulse ", cmd_set_start),
+	SHELL_CMD(stop, NULL, "Stop generating the PWM pulse", cmd_get_stop),
+	SHELL_SUBCMD_SET_END);
+
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_cmd,
+	SHELL_CMD(pwm, &sub_pwm, "Set/Get for PWM", NULL),
+	SHELL_CMD(test, &sub_test, "Test command", NULL),
+	SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(kama, &sub_cmd, "KamaCode shell interface", NULL);
